76fc58b523f0d492c13d27fea654bffda0d1293e
saosaole <102584486+saosaole@users.noreply.github.com>
2022-03-29 19:06:53 +0700

    Assignment2

---
+++ Assets/Scripts/HumanManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour
+{
+    public GameObject human;
+    public float value;
+    public Vector3 sizeChange;
+
+    public void MoveLeft() { 
+        value = value - 0.1f;
+        human.transform.position = new Vector3(value,0,0);
+    }
+    public void RotateLeft() {
+        human.transform.Rotate(0f,0f,20f);
+    }
+    public void Grow() {
+        human.transform.localScale = human.transform.localScale + sizeChange;
+    }
+    public void ResetHuman() {
+        human.transform.position = new Vector3(0,0,0);
+        human.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+        human.transform.localScale = new Vector3(1,1,1);
+    }
+}
